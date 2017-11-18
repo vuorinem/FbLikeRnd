e95ab34a750268f941019f2949af0dd1906ca077
Mikko Vuorinen <vuorinem@gmail.com>
2017-11-18 23:31:32 +0000

    Configure router with single Home route

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,14 @@
-export class App {
-  message = 'Hello World!';
+import { PLATFORM } from 'aurelia-framework';
+import { ConfiguresRouter, RouterConfiguration, Router } from 'aurelia-router'
+
+export class App implements ConfiguresRouter {
+  public configureRouter(config: RouterConfiguration, router: Router): void | Promise<void> | PromiseLike<void> {
+    config.map([
+      {
+        moduleId: PLATFORM.moduleName('views/home'),
+        name: 'Home',
+        route: '',
+      },
+    ]);
+  }
 }
