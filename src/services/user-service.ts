528bd656cc2a8d0d7ea1880451bf553b84817eab
Mikko Vuorinen <vuorinem@gmail.com>
2017-11-19 03:15:37 +0000

    Make user name and id public in UserService

--- src/services/user-service.ts
+++ src/services/user-service.ts
@@ -4,62 +4,70 @@ import { FbService } from "services/fb-service";
 
 @autoinject
 export class UserService {
-    private authResponse: any;
-    
-    private id: string | undefined;
-    private name: string | undefined;
+  public id: string | undefined;
+  public name: string | undefined;
 
-    @computedFrom('authResponse')
-    private get isAuthenticated(): boolean {
-        return this.authResponse.status === 'connected';
+  private authResponse: any;
+
+  @computedFrom('authResponse')
+  private get isAuthenticated(): boolean {
+    return this.authResponse.status === 'connected';
+  }
+
+  constructor(private fbService: FbService) {
+    // No-op
+  }
+
+  public initialize(): Promise<void> {
+    return new Promise(async (resolve, reject) => {
+      await this.fbService.initialize();
+
+      this.fbService.FB.getLoginStatus(async response => {
+        this.authResponse = response;
+
+        await this.updateUserDetails();
+
+        resolve();
+      });
+    });
+  }
+
+  public login(): Promise<void> {
+    return new Promise((resolve, reject) => {
+      const handleResponse = async response => {
+        this.authResponse = response;
+
+        await this.updateUserDetails();
+
+        resolve();
+      }
+
+      this.fbService.FB.login(handleResponse, {
+        scope: 'manage_pages,pages_show_list',
+      });
+    });
+  }
+
+  public fbApi(endpoint: string): Promise<any> {
+    return new Promise((resolve, reject) => {
+      this.fbService.FB.api(endpoint, response => {
+        resolve(response);
+      });
+    });
+  }
+
+  private updateUserDetails(): Promise<void> {
+    if (!this.isAuthenticated) {
+      this.name = undefined;
     }
 
-    constructor(private fbService: FbService) {
-        // No-op
-    }
+    return new Promise((resolve, reject) => {
+      this.fbService.FB.api('/me', response => {
+        this.id = response.id;
+        this.name = response.name;
 
-    public initialize(): Promise<void> {
-        return new Promise(async (resolve, reject) => {
-            await this.fbService.initialize();
-
-            this.fbService.FB.getLoginStatus(async response => {
-                this.authResponse = response;
-
-                await this.updateUserDetails();
-
-                resolve();
-            });
-        });
-    }
-
-    public login(): Promise<void> {
-        return new Promise((resolve, reject) => {
-            const handleResponse = async response => {
-                this.authResponse = response;
-
-                await this.updateUserDetails();
-
-                resolve();
-            }
-
-            this.fbService.FB.login(handleResponse, {
-                scope: 'manage_pages,pages_show_list',
-            });
-        });
-    }
-
-    private updateUserDetails(): Promise<void> {
-        if (!this.isAuthenticated) {
-            this.name = undefined;
-        }
-
-        return new Promise((resolve, reject) => {
-            this.fbService.FB.api('/me', response => {
-                this.id = response.id;
-                this.name = response.name;
-
-                resolve();
-            });
-        });
-    }
+        resolve();
+      });
+    });
+  }
 }
