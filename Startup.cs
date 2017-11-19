a7c4869818491901064c43343e82a9ed68500e09
Mikko Vuorinen <vuorinem@gmail.com>
2017-11-19 16:39:47 +0000

    Auto-format C# code and templates

--- Startup.cs
+++ Startup.cs
@@ -9,57 +9,47 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace WebApplicationBasic
-{
-    public class Startup
-    {
+namespace WebApplicationBasic {
+  public class Startup {
 
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+    public Startup(IConfiguration configuration) {
+      Configuration = configuration;
+    }
 
-        public IConfiguration Configuration { get; }
+    public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            // Add framework services.
-            services.AddMvc();
-        }
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services) {
+      // Add framework services.
+      services.AddMvc();
+    }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseWebpackDevMiddleware(new WebpackDevMiddlewareOptions
-                {
-                  HotModuleReplacement = true, 
-                  ConfigFile="webpack.netcore.config.js",
-                  HotModuleReplacementClientOptions = new Dictionary<string,string>{
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory) {
+      if (env.IsDevelopment()) {
+        app.UseDeveloperExceptionPage();
+        app.UseWebpackDevMiddleware(new WebpackDevMiddlewareOptions {
+          HotModuleReplacement = true,
+          ConfigFile = "webpack.netcore.config.js",
+          HotModuleReplacementClientOptions = new Dictionary<string, string>{
                     {"reload", "true"}
                   }
-                });
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
+        });
+      } else {
+        app.UseExceptionHandler("/Home/Error");
+      }
 
-            app.UseStaticFiles();
+      app.UseStaticFiles();
 
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+      app.UseMvc(routes => {
+        routes.MapRoute(
+            name: "default",
+            template: "{controller=Home}/{action=Index}/{id?}");
 
-                routes.MapSpaFallbackRoute(
-                    name: "spa-fallback",
-                    defaults: new { controller = "Home", action = "Index" });
-            });
-        }
+        routes.MapSpaFallbackRoute(
+            name: "spa-fallback",
+            defaults: new { controller = "Home", action = "Index" });
+      });
     }
+  }
 }
